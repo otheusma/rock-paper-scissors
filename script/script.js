7115d6b565f82816070fa5561b6451c699ac001c
Matheus <matheusm442@gmail.com>
2022-08-02 10:20:26 -0300

    Add javascript to the page

--- script/script.js
+++ script/script.js
@@ -1,78 +1,89 @@
 let playerPoints = 0;
 let computerPoints = 0;
+const playerCards = document.querySelectorAll(".playerCard");
+const middleBoard = document.querySelector(".middleBoard");
+const playerCardChoice = document.querySelector("#playerChoice");
+const computerCardChoice = document.querySelector("#computerChoice");
+const computerCardImage = document.querySelector("#computerImage");
+const playerCardImage = document.querySelector("#playerImage");
+const resultText = document.querySelector("#resultText");
+const playerScore = document.querySelector("#playerScore");
+const computerScore = document.querySelector("#computerScore");
 
-game();
-
-function game() {
-    while(playerPoints != 5 && computerPoints != 5) {
-        playRound(getPlayerChoice(), getComputerChoice());
+playerCards.forEach(card => card.addEventListener("click", () => {
+    playRound(getPlayerChoice(card.id), getComputerChoice())
+    if (playerPoints === 5 || computerPoints === 5) {
+        endGame();
     }
-    showWinner();
-}
+}));
 
-function showWinner() {
-    playerPoints === 5 ? alert("You Win! :)") : alert("You Lose! :(")
+function getPlayerChoice(choice) {
+    switch(choice) {
+        case "rock":
+            playerCardImage.setAttribute("src", "/img/rock");
+            return "Rock";
+        case "paper":
+            playerCardImage.setAttribute("src", "/img/paper");
+            return "Paper"
+        case "scissors":
+            playerCardImage.setAttribute("src", "/img/scissors");
+            return "Scissors"
+    }
 }
-
 function getComputerChoice() {
     switch(Math.floor(Math.random()*3)){
         case 0:
+            computerCardImage.setAttribute("src", "/img/rock");
             return "Rock";
-            break;
         case 1:
+            computerCardImage.setAttribute("src", "/img/paper");
             return "Paper";
-            break;
         case 2:
+            computerCardImage.setAttribute("src", "/img/scissors");
             return "Scissors"
-            break;
     }
 }
-
-function getPlayerChoice() {
-    return capitalizePlayerChoice(prompt("Do you want to choose Rock, Paper or Scissors? "));
-}
-
-function capitalizePlayerChoice(playerChoice) {
-    playerChoice = playerChoice.toLowerCase();
-    return playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
-}
-
-function displayScore() {
-    return `\nPlayer: ${playerPoints} pts.\nComputer: ${computerPoints} pts.`
-}
-
 function playRound(playerChoice, computerChoice) {
+    playerCardChoice.innerText = playerChoice;
+    computerCardChoice.innerText = computerChoice;
     if (playerChoice === computerChoice) {
-        alert("Draw!");
+        resultText.innerText = "Draw!";
     }
     else if (playerChoice === "Rock") {
         if (computerChoice === "Scissors") {
             playerPoints++;
-            alert("You Win! Rock beats Scissors" + displayScore());
+            resultText.innerText = "You Win!\nRock beats Scissors";
         }
         else {
             computerPoints++;
-            alert("You Lose! Paper beats Rock" + displayScore());
+            resultText.innerText = "You Lose!\nPaper beats Rock";
         }
     }
-    else if (playerChoice === "Scissors") {
-        if (computerChoice === "Paper") {
-            playerPoints++;
-            alert("You Win! Scissors beats Paper" + displayScore());
-        }
-        else {
-            computerPoints++;
-            alert("You Lose! Rock beats Scissors" + displayScore());
-        }
-    }
-    else {
+    else if (playerChoice === "Paper") {
+        
         if (computerChoice === "Rock") {
             playerPoints++;
-            alert("You Win! Paper beats Rock" + displayScore());
+            resultText.innerText = "You Win!\nPaper beats Rock";
+        } 
+        else {
+            resultText.innerText = "You Lose!\nScissors beats Paper";
+            computerPoints++;
+        } 
+    }
+    else {
+        if (computerChoice === "Paper") {
+            playerPoints++;
+            resultText.innerText = "You Win!\nScissors beats Paper";
         }
         else {
             computerPoints++;
-            alert("You Lose! Scissors beats Paper" + displayScore());
+            resultText.innerText = "You Lose!\nRocks beats Scissors";
         }
     }
+    playerScore.innerText = playerPoints;
+    computerScore.innerText = computerPoints;
+    middleBoard.style.opacity = "100%";
+}
+function endGame() {
+    playerPoints === 5 ? location.href = "/pages/winScreen.html" : location.href = "/pages/loseScreen.html";
 }
